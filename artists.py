c5c5fb05cea03703261423bbd437871a511813a8
Peter Keenan <keenan@posteo.net>
2020-07-14 14:52:32 +0100

    Scraping quotes; first working version

--- artists.py
+++ artists.py
@@ -1,46 +1,55 @@
 #!/usr/bin/env python3
 
-#   A brief webscraping script to summarise/report on defined
-#   elements on each Samling Artist profile page.
-#   For further information, see the README.
+"""
+This is a brief webscrapring script to summarise/report on
+defined elements on eac Samling Artist profile page.
+
+For further information, see the README.
+"""
 
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 
-page_url = "http://www.samling.org.uk/samling-artist-programme/artists/"
-uClient = uReq(page_url)
+MAIN_URL = "http://www.samling.org.uk/samling-artist-programme/artists/"
+uClient = uReq(MAIN_URL)
 page_html = uClient.read()
 uClient.close()
 page_soup = soup(page_html, "html.parser")
 musicians = page_soup.find_all("h1", {"class": "artist__name"})
 
-out_filename = "sap_artists.csv"
-headers = "Name,URL,Voice-type/Instrument,SAP-Year,IMG \n"
+OUT_FILENAME = "sap_artists.csv"
+HEADERS = "Name,URL,Voice-type/Instrument,SAP-Year,IMG,Quote\n"
 
-f = open(out_filename, "w")
-f.write(headers)
+f = open(OUT_FILENAME, "w")
+f.write(HEADERS)
 
 for musician in musicians:
-    name = str(musician.text.strip())
-    link = str(musician.a["href"])
-    tClient = uReq(link)
+    NAME = str(musician.text.strip())
+    LINK = str(musician.a["href"])
+    tClient = uReq(LINK)
     musician_html = tClient.read()
     tClient.close()
     musician_soup = soup(musician_html, "html.parser")
-    head_text = musician_soup.h2.text
+    HEAD_TEXT = musician_soup.h2.text
     if musician_soup.article.div.img is not None:
         IMG = "Yes"
     else:
         IMG = "No"
+    if musician_soup.div.blockquote is not None:
+        QUOTE = str(musician_soup.div.blockquote.text)
+    else:
+        QUOTE = "No"
     f.write(
-        name
+        NAME
         + ","
-        + link
+        + LINK
         + ","
-        + head_text.replace(", Samling Artist Programme: ", ",")
+        + HEAD_TEXT.replace(", Samling Artist Programme: ", ",")
         + ","
         + IMG
+        + ","
+        + QUOTE.replace(",", "[comma]")
         + "\n"
     )
 
